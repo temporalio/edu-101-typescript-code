a34af50f3cc47f09fbe271f350e21b19a5719283
Kim Schlesinger <kimberly.schlesinger@temporal.io>
2023-01-31 19:02:09 -0700

    update ts files

--- demos/service-workflow/src/workflows.ts
+++ demos/service-workflow/src/workflows.ts
@@ -3,26 +3,15 @@ import type * as activities from './activities';
 
 const {getSpanishGreeting, getSpanishFarewell} = proxyActivities<typeof activities>({
   startToCloseTimeout: '10 seconds',
-})
-
-// const {
-//   getSpanishGreeting,
-//   // completeSomethingAsync,
-// } = proxyActivities<typeof activities>({
-//   retry: {
-//     initialInterval: '50 milliseconds',
-//     maximumAttempts: 2,
-//   },
-//   startToCloseTimeout: '30 seconds',
-// });
+});
 
 export async function greeting(name:string): Promise<string> {
-  const response = await getSpanishGreeting(name)
-  return response
+  const response = await getSpanishGreeting(name);
+  return response;
 }
 
 export async function farewell(name: string): Promise<string> {
-  const response = await getSpanishFarewell(name)
-  return response
+  const response = await getSpanishFarewell(name);
+  return response;
 }
 
